d53327ae653a4e6e89d435fa4f666c5a8071605f
UbaidGhazi <ubaidghazi62m@gmail.com>
2025-08-11 16:29:12 +0500

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns 
 import matplotlib.pyplot as plt
 import plotly.express as px
 from sklearn.model_selection import train_test_split
